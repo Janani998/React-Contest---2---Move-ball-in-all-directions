b06fe9c0c9798687c3fee879452014b9f38f8d79
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-10-25 22:04:40 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, { Component, useState, useEffect } from "react";
+import "../styles.App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: "0px" });
+    setRenderBall(false);
+  };
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      if (event.keyCode === 39) {
+        setX(x + 5);
+      }
+      if (event.keyCode === 37) {
+        setX(x - 5);
+      }
+      if (event.keyCode === 38) {
+        setY(y - 5);
+      }
+      if (event.keyCode === 40) {
+        setY(y + 5);
+      }
+      setBallPosition({ left: `${x}px`, top: `${y}px` });
+    };
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [x, y]);
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={buttonClickHandler} className="start">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
